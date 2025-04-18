6752266364fd96f56e85656abdb9848bd4b74a89
Ayush Singh <ayush@beagleboard.org>
2025-04-18 23:01:50 +0530

    rust: kernel: Add sysfs support
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -44,6 +44,7 @@
 #include <linux/cpufreq.h>
 #include <linux/cpumask.h>
 #include <linux/cred.h>
+#include <linux/device.h>
 #include <linux/device/faux.h>
 #include <linux/dma-mapping.h>
 #include <linux/errname.h>
